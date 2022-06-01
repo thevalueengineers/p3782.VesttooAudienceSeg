d798a31620af89b880ce671c2548c305ed77be7a
--replace-all <alan.clark@thevalueengineers.com>
2022-06-01 16:41:34 -0700

    jun1 third option

--- p3782.vesttoo.r
+++ p3782.vesttoo.r
@@ -14,7 +14,7 @@ coal <- function(arg1){
 }
 
 pcts <- function(arg1,arg2,arg3){
-  arg1 %>%df$a4r8 filter(!is.na({{arg2}})) %>% group_by({{arg2}},{{arg3}}) %>% summarise(n=n()) %>% mutate(pct=100*(n/sum(n)))}
+  arg1 %>% filter(!is.na({{arg2}})) %>% group_by({{arg2}},{{arg3}}) %>% summarise(n=n()) %>% mutate(pct=100*(n/sum(n)))}
 
 #load data
 setwd("C:/Work/p3782.VesttooAudienceSeg")
@@ -27,23 +27,48 @@ clust1 <- df %>% select(clustv1)
 k1<-kmeans(clust1, centers=5, iter.max = 1000, nstart=5)
 k1
 df$cluster1 <- k1$cluster
+clust_stats_1<-df %>% select(cluster1,ilsfam,decider,a1ar3,a1ar7,newmedia,contains('a1'), contains('a2'),contains('a3'),contains('a4'),contains('a5'),contains('c1r'),contains('c7r'),contains('c8r')) %>% select(-contains('oe'),-a4r96,-noanswera4_r99) %>% group_by(cluster1) %>% summarise(across(everything(),mean))
+write_csv(clust_stats_1,'clust1.csv')
 
 #'roi','tech','ai','future','subs''wordofmouth','newinv'
 clustv2 <- c('ai','ilsfam','newinv')
-clust1 <- df %>% select(clustv2)
+clust2 <- df %>% select(clustv2)
 k2<-kmeans(clust2, centers=5, iter.max = 1000, nstart=5)
 k2
 df$cluster2 <- k2$cluster
+clust_stats_2<-df %>% select(cluster2,ai,ilsfam,newinv,contains('a1'), contains('a2'),contains('a3'),contains('a4'),contains('a5'),contains('c1r'),contains('c7r'),contains('c8r')) %>% select(-contains('oe'),-a4r96,-noanswera4_r99) %>% group_by(cluster2) %>% summarise(across(everything(),mean))
+write_csv(clust_stats_2,'clust2.csv')
+
+clustv3 <- c('a1br4','a1br5','a1ar7','a1cr4','a1cr6','a5r7','a5r8','a1br2','a4r7','a4r8','a3r8','a1cr7','a4r3','a1cr5')
+clust3 <- df %>% select(clustv3) %>% mutate(a5r7=2*(a5r7-1)+1,a5r8=2*(a5r8-1)+1,a3r8=2*(a3r8-1)+1,a4r3=a4r3*3,a4r7=a4r7*3,a4r8=a4r8*3)
+clust3 <- clust3 %>% mutate(lookingfor=(a4r8+a3r8),interest=(a1cr4+a1cr6+(10-a5r8))/3,techai=(a1br2+a1br4)/2,a1br5=as.double(a1br5),a1ar7=as.double(a1ar7)) %>% select(-a4r7,-a5r7,-a1cr5,-a4r3,-a1cr7) %>% select(-a1cr4,-a1cr6,-a5r8,-a1br4,-a1br2,-a4r8,-a3r8)
+k3<-kmeans(clust3, centers=5, iter.max = 1000, nstart=5)
+k3
+df$cluster3 <- k3$cluster
+clust_stats_3<-df %>% select(cluster3,contains('a1'), contains('a2'),contains('a3'),contains('a4'),contains('a5'),contains('c1r'),contains('c7r'),contains('c8r')) %>% select(-contains('oe'),-a4r96,-noanswera4_r99) %>% group_by(cluster3) %>% summarise(across(everything(),mean))
+write_csv(clust_stats_3,'clust3.csv')
+
+library(factoextra)
+library(cluster)
+clustm <- pam(clust3,k=5,metric='euclidean',stand=FALSE)
+clustm
+fviz_nbclust(clust3,pam,method='wss')
+gap_stat <- clusGap(clust3,FUN=pam,K.max=8,B=50)
+fviz_gap_stat(gap_stat)
+
+write_csv(df,'vesttoo_with_clusters.csv')
+write_sav(df,'vesttoo_with_clusters.sav')
 
 #cb(k$centers)
-cor(clust)
+cor(clust1)
+cor(clust2)
 #clust2 <- df %>% select(contains('a1a')) #%>% select(-contains('oe'),-a4r96,-noanswera4_r99)
 #clust2 <- df %>% select(contains('a1'), contains('a4'),contains('a5')) %>% select(-contains('oe'),-a4r96,-noanswera4_r99) #mutate(across(everything(),coal))
 #View(cor(clust2))
 
-f <- fa(clust2,nfactors=8, rotate='oblimin',fm='minres')
-f
-print(f$loadings,cutoff=0.3)
+#f <- fa(clust2,nfactors=8, rotate='oblimin',fm='minres')
+#f
+#print(f$loadings,cutoff=0.3)
 
 #c1r1,c1r4,c1r7,c1r10
 #clust %>% select(!where(is.numeric))
